cf91a14e1cc656f1b8d478be37120d8e7109fc14
Alatise Martins <martinsalatise1@gmail.com>
2026-05-15 12:50:14 +0100

    initial commit: Hello world project

---
+++ intro.js
@@ -0,0 +1,5 @@
+let name = 'Alatise Martins';
+age = 17;
+favouriteProgramming = "javascript, C++"
+
+console.log ('My name is'+ ' ', name ,'i am'+ ' ' ,age + 'yrs old ', 'my favourite Programming is', favouriteProgramming)
